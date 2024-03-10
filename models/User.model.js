3b2ffaf80b5527deb5456a2be1682ea56695bb15
Lara Aguerre <aguerre.lara@gmail.com>
2024-03-10 15:13:45 +0100

    profile picture addded to user

--- models/User.model.js
+++ models/User.model.js
@@ -17,7 +17,7 @@ const userSchema = new Schema(
       type: String,
       required: [true, 'Username es necesario.'],
     },
-    image: {
+    imageUrl: {
       type: String
     },
     birth: {
